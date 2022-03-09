2efc835811fdab825b8131b7a8b441ab04fedacb
t56k <txnash@gmail.com>
2022-03-09 13:57:31 +1300

    add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,44 @@
+use std::collections::HashSet;
+
 pub mod algorithms;
 
-pub fn play<G: Guesser>(answer: &'static str, mut guesser: G) -> Option<usize> {
-    let mut history = vec![];
+const DICTIONARY: &str = include_str!("../dictionary.txt");
 
-    for i in 1..=32 {
-        let guess = guesser.guess(&history);
-        if guess == answer {
-            return Some(i);
+pub struct Wordle {
+    dictionary: HashSet<&'static str>,
+}
+
+impl Wordle {
+    pub fn new() -> Self {
+        Self {
+            dictionary: HashSet::from_iter(DICTIONARY.lines().map(|line| {
+                line.split_once(' ')
+                    .expect("every line is word + space + freq")
+                    .0
+            })),
         }
-
-        let correctness = Correctness::compute(answer, &guess);
-        history.push(Guess {
-            word: guess,
-            mask: correctness,
-        });
     }
 
-    None
+    pub fn play<G: Guesser>(&self, answer: &'static str, mut guesser: G) -> Option<usize> {
+        let mut history = vec![];
+
+        for i in 1..=32 {
+            let guess = guesser.guess(&history);
+            if guess == answer {
+                return Some(i);
+            }
+
+            assert!(self.dictionary.contains(&*guess));
+
+            let correctness = Correctness::compute(answer, &guess);
+            history.push(Guess {
+                word: guess,
+                mask: correctness,
+            });
+        }
+
+        None
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -77,8 +99,104 @@ pub trait Guesser {
     fn guess(&mut self, history: &[Guess]) -> String;
 }
 
+impl Guesser for fn(history: &[Guess]) -> String {
+    fn guess(&mut self, history: &[Guess]) -> String {
+        (*self)(history)
+    }
+}
+
+#[cfg(test)]
+macro_rules! guesser {
+    (|$history:ident| $impl:block) => {{
+        struct G;
+        impl $crate::Guesser for G {
+            fn guess(&mut self, $history: &[Guess]) -> String {
+                $impl
+            }
+        }
+        G
+    }};
+}
+
 #[cfg(test)]
 mod tests {
+    mod game {
+        use crate::{Guess, Wordle};
+
+        #[test]
+        fn genius() {
+            let w = Wordle::new();
+            let guesser = guesser!(|_history| { "right".to_string() });
+
+            assert_eq!(w.play("right", guesser), Some(1));
+        }
+
+        #[test]
+        fn magnificent() {
+            let w = Wordle::new();
+            let guesser = guesser!(|history| {
+                if history.len() == 1 {
+                    return "right".to_string();
+                }
+                return "wrong".to_string();
+            });
+
+            assert_eq!(w.play("right", guesser), Some(2));
+        }
+
+        #[test]
+        fn impressive() {
+            let w = Wordle::new();
+            let guesser = guesser!(|history| {
+                if history.len() == 2 {
+                    return "right".to_string();
+                }
+                return "wrong".to_string();
+            });
+
+            assert_eq!(w.play("right", guesser), Some(3));
+        }
+
+        #[test]
+        fn splendid() {
+            let w = Wordle::new();
+            let guesser = guesser!(|history| {
+                if history.len() == 3 {
+                    return "right".to_string();
+                }
+                return "wrong".to_string();
+            });
+
+            assert_eq!(w.play("right", guesser), Some(4));
+        }
+
+        #[test]
+        fn great() {
+            let w = Wordle::new();
+            let guesser = guesser!(|history| {
+                if history.len() == 4 {
+                    return "right".to_string();
+                }
+                return "wrong".to_string();
+            });
+
+            assert_eq!(w.play("right", guesser), Some(5));
+        }
+
+        #[test]
+        fn phew() {
+            let w = Wordle::new();
+            let guesser = guesser!(|history| {
+                if history.len() == 5 {
+                    return "right".to_string();
+                }
+                return "wrong".to_string();
+            });
+
+            assert_eq!(w.play("right", guesser), Some(6));
+        }
+    }
+
     mod compute {
         use crate::Correctness;
 
@@ -93,50 +211,32 @@ mod tests {
 
         #[test]
         fn all_green() {
-            assert_eq!(
-                Correctness::compute("abcde", "abcde"),
-                mask![C C C C C]
-            );
+            assert_eq!(Correctness::compute("abcde", "abcde"), mask![C C C C C]);
         }
 
         #[test]
         fn all_grey() {
-            assert_eq!(
-                Correctness::compute("zxywq", "abcde"),
-                mask![W W W W W]
-            );
+            assert_eq!(Correctness::compute("zxywq", "abcde"), mask![W W W W W]);
         }
 
         #[test]
         fn all_yellow() {
-            assert_eq!(
-                Correctness::compute("cedba", "abcde"),
-                mask![M M M M M]
-            );
+            assert_eq!(Correctness::compute("cedba", "abcde"), mask![M M M M M]);
         }
 
         #[test]
         fn repeat_green() {
-            assert_eq!(
-                Correctness::compute("aaabb", "aaacc"),
-                mask![C C C W W]
-            );
+            assert_eq!(Correctness::compute("aaabb", "aaacc"), mask![C C C W W]);
         }
 
         #[test]
         fn repeat_yellow() {
-            assert_eq!(
-                Correctness::compute("cccaa", "aaacc"),
-                mask![M M W M M]
-            );
+            assert_eq!(Correctness::compute("cccaa", "aaacc"), mask![M M W M M]);
         }
 
         #[test]
         fn repeat_some_green() {
-            assert_eq!(
-                Correctness::compute("aabbb", "caacc"),
-                mask![W C M W W]
-            );
+            assert_eq!(Correctness::compute("aabbb", "caacc"), mask![W C M W W]);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 const GAMES: &str = include_str!("../answers.txt");
 
 fn main() {
+    let w = hurdle::Wordle::new();
+
     for answer in GAMES.split_whitespace() {
         let guesser = hurdle::algorithms::Naive::new();
-        hurdle::play(answer, guesser);
+        w.play(answer, guesser);
     }
 }
